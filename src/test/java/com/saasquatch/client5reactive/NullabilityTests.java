9df6ac0eb9d114e10ddc9cb89a10c5c8c796b3d6
slisaasquatch <sli@saasquat.ch>
2020-02-28 13:18:51 -0800

    SL: more tests

--- src/test/java/com/saasquatch/client5reactive/NullabilityTests.java
+++ src/test/java/com/saasquatch/client5reactive/NullabilityTests.java
@@ -2,12 +2,17 @@ package com.saasquatch.client5reactive;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequests;
+import org.apache.hc.client5.http.async.methods.SimpleRequestProducer;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.core5.http.nio.AsyncRequestProducer;
+import org.apache.hc.core5.reactive.ReactiveResponseConsumer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import io.reactivex.rxjava3.core.Flowable;
 
 public class NullabilityTests {
 
@@ -42,6 +47,14 @@ public class NullabilityTests {
         () -> reactiveClient.streamingExecute((AsyncRequestProducer) null, null));
     assertThrows(NullPointerException.class,
         () -> reactiveClient.streamingExecute((AsyncRequestProducer) null));
+  }
+
+  @Test
+  public void testEmptyPublisher() {
+    final Publisher<Void> voidResultPublisher = reactiveClient.execute(
+        SimpleRequestProducer.create(SimpleHttpRequests.get("https://example.com")),
+        new ReactiveResponseConsumer());
+    Flowable.fromPublisher(voidResultPublisher).test().assertEmpty();
   }
 
 }
