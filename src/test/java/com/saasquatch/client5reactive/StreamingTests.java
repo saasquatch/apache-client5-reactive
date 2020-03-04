2841e14937d97030cffcc04f7e5167b636e41ce4
slisaasquatch <sli@saasquat.ch>
2020-03-04 11:54:41 -0800

    remove Guava as dependency

--- src/test/java/com/saasquatch/client5reactive/StreamingTests.java
+++ src/test/java/com/saasquatch/client5reactive/StreamingTests.java
@@ -1,8 +1,12 @@
 package com.saasquatch.client5reactive;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import java.net.URL;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequests;
 import org.apache.hc.client5.http.async.methods.SimpleRequestProducer;
 import org.apache.hc.client5.http.async.methods.SimpleResponseConsumer;
@@ -12,13 +16,12 @@ import org.apache.hc.core5.http.Message;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import com.google.common.io.ByteStreams;
-import com.google.common.primitives.Bytes;
+import org.reactivestreams.Publisher;
 import io.reactivex.rxjava3.core.Flowable;
 
 public class StreamingTests {
 
-  private static final String FLOWABLE_URL =
+  private static final String FLOWABLE_SOURCE_URL =
       "https://cdn.jsdelivr.net/gh/ReactiveX/RxJava@81f0569a8b9b7d27059f127b90fd7335118b2ee4/src/main/java/io/reactivex/rxjava3/core/Flowable.java";
   private static CloseableHttpAsyncClient asyncClient;
   private static ReactiveHttpAsyncClient reactiveClient;
@@ -29,7 +32,8 @@ public class StreamingTests {
     asyncClient = HttpAsyncClients.createDefault();
     asyncClient.start();
     reactiveClient = ReactiveHttpAsyncClients.create(asyncClient);
-    flowableSourceBytes = ByteStreams.toByteArray(new URL(FLOWABLE_URL).openStream());
+    flowableSourceBytes =
+        asyncClient.execute(SimpleHttpRequests.get(FLOWABLE_SOURCE_URL), null).get().getBodyBytes();
   }
 
   @AfterAll
@@ -39,10 +43,9 @@ public class StreamingTests {
 
   @Test
   public void testVanillaExecuteWorks() {
-    final byte[] bodyBytes = Flowable.fromPublisher(
-        reactiveClient.execute(SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_URL)),
-            SimpleResponseConsumer.create()))
-        .blockingSingle().getBodyBytes();
+    final byte[] bodyBytes = Flowable.fromPublisher(reactiveClient.execute(
+        SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_SOURCE_URL)),
+        SimpleResponseConsumer.create())).blockingSingle().getBodyBytes();
     assertArrayEquals(flowableSourceBytes, bodyBytes);
   }
 
@@ -50,26 +53,28 @@ public class StreamingTests {
   public void testVanillaStreamingWorks() {
     final byte[] bodyBytes = Flowable
         .fromPublisher(reactiveClient.streamingExecute(
-            SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_URL)), null))
-        .concatMap(Message::getBody).map(bb -> byteBufferToArray(bb)).reduce(Bytes::concat)
-        .blockingGet();
+            SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_SOURCE_URL)), null))
+        .concatMap(Message::getBody).to(this::toByteArray);
     assertArrayEquals(flowableSourceBytes, bodyBytes);
   }
 
   @Test
   public void testBasicStreamingWorks() {
     final byte[] bodyBytes = Flowable
-        .fromPublisher(reactiveClient
-            .streamingExecute(SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_URL))))
-        .concatMap(Message::getBody).map(bb -> byteBufferToArray(bb)).reduce(Bytes::concat)
-        .blockingGet();
+        .fromPublisher(reactiveClient.streamingExecute(
+            SimpleRequestProducer.create(SimpleHttpRequests.get(FLOWABLE_SOURCE_URL))))
+        .concatMap(Message::getBody).to(this::toByteArray);
     assertArrayEquals(flowableSourceBytes, bodyBytes);
   }
 
-  private static byte[] byteBufferToArray(ByteBuffer bb) {
-    final byte[] arr = new byte[bb.remaining()];
-    bb.get(arr);
-    return arr;
+  private byte[] toByteArray(Publisher<ByteBuffer> pub) {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+        WritableByteChannel channel = Channels.newChannel(out);) {
+      Flowable.fromPublisher(pub).blockingForEach(channel::write);
+      return out.toByteArray();
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 
 }
